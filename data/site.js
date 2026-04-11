46dfa88c5d8d7e96739035e3836d9b80eaf2e9f9
Slabnevitch <handofschpilrain@mail.ru>
2026-04-11 19:22:16 +0500

    начал шапку

--- data/site.js
+++ data/site.js
@@ -1,4 +1,10 @@
 export default {
 	path: "/src/data/site.js",
-	dogs: ["Белка", "Зина", "Кукла", "Цыган", "Чип"]
+	dogs: ["Белка", "Зина", "Кукла", "Цыган", "Чип"],
+	mainMenu: [
+		{name: 'services', url: 'services.html'},
+		{name: 'work', url: 'work.html'},
+		{name: 'about', url: 'about.html'},
+		{name: 'blog', url: 'blog.html'}
+	]
 };
