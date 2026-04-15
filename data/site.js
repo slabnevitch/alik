b385be8e1d23d90b14fa6fce2992ac0559f0d771
Slabnevitch <handofschpilrain@mail.ru>
2026-04-15 13:09:35 +0500

    заверстал .skills

--- data/site.js
+++ data/site.js
@@ -6,5 +6,23 @@ export default {
 		{name: 'work', url: 'work.html'},
 		{name: 'about', url: 'about.html'},
 		{name: 'blog', url: 'blog.html'}
+	],
+	skills: [
+		{
+			title: 'Web design',
+			text: 'Visually stunning web designs that captivate your audience by blending your brand voice and customer needs.',
+			btn: 'About Webdesign'
+		},
+		{
+			title: 'Development',
+			text: 'Get custom web development solutions that are tailored to your specifications, designed to deliver a flawless user experience.',
+			btn: 'About Webflow'
+		},
+		{
+			title: 'Content & Seo',
+			text: 'Proven SEO strategies that enhance your online performance, bringing you to the forefront of organic search results.',
+			btn: 'About SEO'
+		}
+	
 	]
 };
