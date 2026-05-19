a1935659413b5fd9536928ce7764b492c559295b
Slabnevitch <handofschpilrain@mail.ru>
2026-05-19 20:17:42 +0500

    добавил тренировочно tailwind

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 import 'virtual:svg-to-font.css'
 // import svgSprite from 'virtual:svg-sprite';
 import '@/styles/index.scss';
+// import '@/styles/tailwind.css';
 // import * as bootstrap from 'bootstrap';
 // import './helpers/focus-visible';
 import './helpers/toggle-mnu';
